9f4e83e6abd5dcb0701d4f212187a2062f527fce
Mark Lopez <molopez492@gmail.com>
2018-02-02 16:38:49 -0500

    fix: activate color picker indicator

--- app/containers/App.js
+++ app/containers/App.js
@@ -62,12 +62,12 @@ export default class App extends Component {
   handleSketchChange() {
     const sketchOn = !this.state.sketchOn;
     this.setState({ sketchOn });
-   if(sketchOn){
-    let sketchOnClick = "color: red; outline:1px solid limegreen;"
-    document.getElementById("sketchButton").style.cssText = sketchOnClick;
-   }else{
-    let sketchOffClick = "color:whitesmoke;"
-    document.getElementById("sketchButton").style.cssText = sketchOffClick;
+    if (sketchOn) {
+     const sketchOnClick = 'color: red; outline:1px solid limegreen;';
+     document.getElementById('sketchButton').style.cssText = sketchOnClick;
+   } else{
+     const sketchOffClick = 'color:whitesmoke;';
+     document.getElementById('sketchButton').style.cssText = sketchOffClick;
    }
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, { sketchOn }, (response) => {
@@ -75,20 +75,20 @@ export default class App extends Component {
     });
   }
 
-  handleScreenCapture(){
+  handleScreenCapture() {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       let id = tabs[0].id;
 
-      chrome.tabs.captureVisibleTab(function (screenshotUrl) {
-        const viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
+      chrome.tabs.captureVisibleTab((screenshotUrl) => {
+        const viewTabUrl = chrome.extension.getURL(`screenshot.html?id=${  id++}`);
         let targetId = null;
 
         chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
-          if (tabId != targetId || changedProps.status != "complete")
-            return;
+          if (tabId != targetId || changedProps.status != 'complete')
+            {return;}
           chrome.tabs.onUpdated.removeListener(listener);
           const views = chrome.extension.getViews();
-          for (var i = 0; i < views.length; i++) {
+          for (let i = 0; i < views.length; i++) {
             const view = views[i];
             if (view.location.href == viewTabUrl) {
               view.setScreenshotUrl(screenshotUrl);
@@ -96,7 +96,7 @@ export default class App extends Component {
             }
           }
         });
-        chrome.tabs.create({ url: viewTabUrl }, function (tab) {
+        chrome.tabs.create({ url: viewTabUrl }, (tab) => {
           targetId = tab.id;
         });
       });
@@ -108,27 +108,27 @@ export default class App extends Component {
     return (
       <div className={style.App}>
         <header className={style.appHeader}>
-        <link rel="stylesheet" href="https://unpkg.com/react-tabs@2/style/react-tabs.css" />
+          <link rel="stylesheet" href="https://unpkg.com/react-tabs@2/style/react-tabs.css" />
           <h1 className={style.appTitle}>FrankenStyle</h1>
 
           <div id={style.elementSelector}>
             <div id={style.toggleShrinker}>
               <label className={style.switch}>
-                <input type="checkbox" onChange={()=>{this.handleHighlightChange()}}></input>
-                <span className={[style.slider, style.round].join(" ")}></span>
+                <input type="checkbox" onChange={() => { this.handleHighlightChange(); }} />
+                <span className={[style.slider, style.round].join(' ')} />
               </label>
               <span>Select Element</span>
             </div>
 
             <input type="text" value={this.state.element} id={style.displayImg} />
-          </div>  
+          </div>
           <hr />
           <div className={style.buttons}>
             <button className={style.buttonStyle} id="sketchButton" type="button" onClick={this.handleSketchChange}>
               <img src="/img/sketch.png" alt="Sketch" /> Draw
             </button>
             <button className={style.buttonStyle} type="button" onClick={this.handleScreenCapture}>
-              <img src="/img/camera.png"  alt="Screen Capture" /> Screenshot
+              <img src="/img/camera.png" alt="Screen Capture" /> Screenshot
             </button>
           </div>
 
@@ -146,7 +146,7 @@ export default class App extends Component {
           <TabPanel>
             <form id={style.colorForm}>
               <h2 className={style.selectColorTitle}>Select Background Color</h2>
-              <SketchPicker color={this.state.fontColor} onChange={this.handleBackgroundColorChange} />
+              <SketchPicker color={this.state.backgroundColor} onChange={this.handleBackgroundColorChange} />
             </form>
           </TabPanel>
           <TabPanel>
