945aad5415d0582990f5f778b851478ee85f71c3
Mark Lopez <molopez492@gmail.com>
2018-02-03 17:43:10 -0500

    feat: add font color picker

---
+++ app/components/ColorPicker.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import reactCSS from 'reactcss';
+import { SketchPicker } from 'react-color';
+
+export default class ColorPicker extends React.Component {
+  state = {
+    displayColorPicker: false,
+  };
+
+  handleClick = () => {
+    this.setState({ displayColorPicker: !this.state.displayColorPicker });
+  };
+
+  handleClose = () => {
+    this.setState({ displayColorPicker: false });
+  };
+
+  render() {
+    const { onChangeHandler, color } = this.props;
+    const styles = reactCSS({
+      default: {
+        color: {
+          width: '36px',
+          height: '14px',
+          borderRadius: '2px',
+          background: `${color}`,
+        },
+        swatch: {
+          padding: '5px',
+          background: '#fff',
+          borderRadius: '1px',
+          boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+          display: 'inline-block',
+          cursor: 'pointer',
+        },
+        popover: {
+          position: 'absolute',
+          zIndex: '2',
+        },
+        cover: {
+          position: 'fixed',
+          top: '0px',
+          right: '0px',
+          bottom: '0px',
+          left: '0px',
+        },
+      },
+    });
+
+    return (
+      <div>
+        <div style={styles.swatch} onClick={this.handleClick}>
+          <div style={styles.color} />
+        </div>
+        {this.state.displayColorPicker ? <div style={styles.popover}>
+          <div style={styles.cover} onClick={this.handleClose} />
+          <SketchPicker color={color} onChange={onChangeHandler} />
+        </div> : null}
+
+      </div>
+    );
+  }
+}
