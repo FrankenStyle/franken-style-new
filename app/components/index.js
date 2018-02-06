c86e533dda13731a91888a472cbd3770d284ab3f
Vesna Tan <tanvezi@gmail.com>
2018-02-06 15:49:08 -0500

    refactor: changed index to have texts
    refactor: changed index to have texts

--- app/components/index.js
+++ app/components/index.js
@@ -2,4 +2,4 @@ export { default as Borders } from './Borders';
 export { default as Button } from './Button';
 export { default as Colors } from './Colors';
 export { default as Flexbox } from './Flexbox';
-
+export { default as Texts } from './Texts';
